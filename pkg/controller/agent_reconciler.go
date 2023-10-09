a600c4cf5a432eba75902a2eb125c7f03e9c6271
Tomer Figenblat <tfigenbl@redhat.com>
2023-10-08 22:15:08 -0400

    chore: added code for the the addon reconciliation loop
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>

--- pkg/controller/agent_reconciler.go
+++ pkg/controller/agent_reconciler.go
@@ -6,7 +6,10 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	addonv1alpha1 "open-cluster-management.io/api/addon/v1alpha1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -37,10 +40,46 @@ type AgentReconciler struct {
 
 func (r *AgentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
-	logger.Info("TODO-ADD-AGENT-RECONCILE-LOGIC")
-	// TODO
 
-	return ctrl.Result{Requeue: false}, nil
+	// fetch the ManagedClusterAddon resource
+	mcAddonNsn := types.NamespacedName{
+		Namespace: req.Namespace,
+		Name:      req.Name,
+	}
+	mcAddon := &addonv1alpha1.ManagedClusterAddOn{}
+	if err := r.Client.Get(ctx, mcAddonNsn, mcAddon); err != nil {
+		if errors.IsNotFound(err) {
+			logger.Info(fmt.Sprintf("%s named not found in namepsace.", mcAddonNsn.String()))
+			return ctrl.Result{}, nil
+		}
+
+		logger.Error(err, "failed to fetch ManagedClusterAddon")
+		return ctrl.Result{}, err
+	}
+
+	// is the ManagedClusterAddon for our addon?
+	if len(mcAddon.OwnerReferences) == 0 ||
+		mcAddon.OwnerReferences[0].Kind != "ClusterManagementAddOn" ||
+		mcAddon.OwnerReferences[0].Name != "multicluster-resiliency-addon" {
+		return ctrl.Result{}, nil
+	}
+
+	// is the ManagedClusterAddon being deleted?
+	if !mcAddon.ObjectMeta.DeletionTimestamp.IsZero() {
+		logger.Info(fmt.Sprintf(
+			"ManagedClusterAddon for %s is being deleted, delting corresponding ResilientCluster",
+			mcAddonNsn.Namespace))
+
+		// TODO delete ResilientCluster
+
+		return ctrl.Result{}, nil
+	}
+
+	// TODO check if ResilientCluster exists
+	// TODO if exists, update current addon state
+	// TODO if not create new and set owner and initial state
+
+	return ctrl.Result{}, nil
 }
 
 // SetupWithManager is used for setting up the controller with the manager.
