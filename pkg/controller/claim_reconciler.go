90294241060be49b145069e622bef1804769e403
Tomer Figenblat <tfigenbl@redhat.com>
2023-10-24 12:13:56 -0400

    chore: more claim reconciler code
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>

--- pkg/controller/claim_reconciler.go
+++ pkg/controller/claim_reconciler.go
@@ -9,7 +9,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
-	"open-cluster-management.io/api/addon/v1alpha1"
+	addonv1alpha1 "open-cluster-management.io/api/addon/v1alpha1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -35,7 +35,7 @@ func (r *ClaimReconciler) SetupWithManager(mgr ctrl.Manager) error {
 
 // +kubebuilder:rbac:groups=hive.openshift.io,resources=clusterpools,verbs=get;list
 // +kubebuilder:rbac:groups=hive.openshift.io,resources=clusterclaims,verbs=get;list;create;watch
-// +kubebuilder:rbac:groups=hive.openshift.io,resources=clusterdeployments,verbs=*
+// +kubebuilder:rbac:groups=hive.openshift.io,resources=clusterdeployments,verbs=get;list;create;delete
 
 // Reconcile is watching ClusterClaim CRs updating the appropriate ResilientCluster CRs, and deleting replaced
 // ClusterClaim CRs. Note, further permissions are listed in ClusterReconciler.Reconcile and AddonReconciler.Reconcile.
@@ -64,49 +64,75 @@ func (r *ClaimReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 		if controllerutil.ContainsFinalizer(claim, finalizerUsedByMcra) {
 			// TODO verify ClusterClaim status before proceeding
 
-			// the old spoke name is the cluster-namespace name
+			// the OLD spoke name was set as an annotation when we created the ClusterClaim in ClusterReconciler
 			oldSpokeName := claim.GetAnnotations()[annotationPreviousSpoke]
-			// the new spoke name is the namespace in which the deployment exists
+			// the NEW spoke name is the target namespace in which the ClusterDeployment was created
 			newSpokeName := claim.Spec.Namespace
 
-			// the ClusterDeployment resource resides in the cluster-namespace and has the same name as its claim
-			deploymentSubject := types.NamespacedName{
+			// the ClusterDeployment resides in the cluster-namespace with a matching name
+			oldDeploymentSubject := types.NamespacedName{
 				Namespace: oldSpokeName,
-				Name:      claim.Name,
+				Name:      oldSpokeName,
 			}
 
-			// fetch ClusterDeployment from previous cluster
-			deployment := &hivev1.ClusterDeployment{}
-			if err := r.Client.Get(ctx, deploymentSubject, deployment); err != nil {
-				// TODO handle errors - don't return without removing annotation and finalizer
+			// fetch ClusterDeployment from previous OLD cluster and delete it if exists
+			oldDeployment := &hivev1.ClusterDeployment{}
+			if err := r.Client.Get(ctx, oldDeploymentSubject, oldDeployment); err != nil {
+				logger.Info(fmt.Sprintf("no ClusterDeployments found on %s", oldSpokeName))
+			} else {
+				if err = r.Client.Delete(ctx, oldDeployment); err != nil {
+					logger.Error(err, fmt.Sprintf("failed deleting ClusterDepolyment %v", oldDeploymentSubject))
+				}
 			}
 
 			// the ManagedClusterAddon resides in the cluster-namespace
-			mcaSubject := types.NamespacedName{
+			oldMcaSubject := types.NamespacedName{
 				Namespace: oldSpokeName,
 				Name:      "multicluster-resiliency-addon",
 			}
 
-			// fetch ManagedClusterAddOn from previous cluster
-			mca := &v1alpha1.ManagedClusterAddOn{}
-			if err := r.Client.Get(ctx, mcaSubject, mca); err != nil {
-				// TODO handle errors - don't return without removing annotation and finalizer
+			// fetch ManagedClusterAddOn from OLD cluster, create a copy in the NEW cluster and delete the OLD one
+			oldMca := &addonv1alpha1.ManagedClusterAddOn{}
+			if err := r.Client.Get(ctx, oldMcaSubject, oldMca); err != nil {
+				logger.Error(err, fmt.Sprintf("failed fetching ManagedClusterAddon %s", oldMcaSubject))
+			} else {
+				newMca := oldMca.DeepCopy()
+
+				newMca.SetName("multicluster-resiliency-addon")
+				newMca.SetNamespace(newSpokeName)
+
+				newMca.SetLabels(oldMca.GetLabels())
+				newMca.SetOwnerReferences(oldMca.GetOwnerReferences())
+				newMca.SetFinalizers(oldMca.GetFinalizers())
+				newMca.SetManagedFields(oldMca.GetManagedFields())
+
+				annotations := oldMca.GetAnnotations()
+				annotations[annotationFromAnnotation] = oldSpokeName
+				newMca.SetAnnotations(annotations)
+
+				if err = r.Client.Create(ctx, newMca); err != nil {
+					logger.Error(err, fmt.Sprintf("failed creating new ManagedClusterAddon in %s", newSpokeName))
+				}
 			}
 
-			// fetch all AddOnDeploymentConfig from the cluster namespace
-			configs := &v1alpha1.AddOnDeploymentConfigList{}
-			if err := r.Client.List(ctx, configs, &client.ListOptions{Namespace: oldSpokeName}); err != nil {
-				// TODO handle errors - don't return without removing annotation and finalizer
+			// fetch AddOnDeploymentConfigs from previous OLD cluster and copy them to the NEW one
+			oldConfigs := &addonv1alpha1.AddOnDeploymentConfigList{}
+			if err := r.Client.List(ctx, oldConfigs, &client.ListOptions{Namespace: oldSpokeName}); err != nil {
+				logger.Info(fmt.Sprintf("no AddOnDeploymentConfigs found on %s", oldSpokeName))
+			} else {
+				for _, oldConfig := range oldConfigs.Items {
+					newConfig := oldConfig.DeepCopy()
+					newConfig.SetName(oldConfig.Name)
+					newConfig.SetNamespace(newSpokeName)
+					if err = r.Client.Create(ctx, newConfig); err != nil {
+						logger.Error(err, fmt.Sprintf("failed creating AddOnDeploymentConfig %s in %s", newConfig.Name, newSpokeName))
+					}
+				}
 			}
-
-			// TODO - delete ClusterDeployment
-			// TODO - copy ManagedClusterAddon to new cluster-namespace
-			// TODO - copy (if any) all AddOnDeploymentConfig to new cluster-namespace
-			// TODO - move replace spoke in ManagedClusterSets (add rbac markers)
 
 			// when done, remove the finalizer and annotation
 			annotations := claim.GetAnnotations()
-			delete(claim.GetAnnotations(), annotationPreviousSpoke)
+			delete(annotations, annotationPreviousSpoke)
 			claim.SetAnnotations(annotations)
 
 			controllerutil.RemoveFinalizer(claim, finalizerUsedByMcra)
